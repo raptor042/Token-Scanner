fb531b56eb50ce21495cae7f70f3d4370d97a85c
Benji Chimex <benjichimex@MacBooks-MacBook-Pro.local>
2024-05-05 19:00:46 +0100

    another commit

--- index.js
+++ index.js
@@ -42,33 +42,54 @@ bot.hears(/^0x/, async ctx => {
             const age = await getAge(_info.pair.pairCreatedAt)
             const block = await getBlock(info.pair.creationTxHash)
             const logs = await getLogs(address, block)
+
             let sniper_text = ""
             let total_sniped_volume = 0
 
-            logs.forEach((log, index) => {
-                if(index == 0) {
-                    sniper_text += `🥇 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
-                    total_sniped_volume += format(info.token.decimals, log.args[2], "div")
-                } else if(index == 1) {
-                    sniper_text += `🥈 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
-                    total_sniped_volume += format(info.token.decimals, log.args[2], "div")
-                } else if(index == 2) {
-                    sniper_text += `🥉 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
-                    total_sniped_volume += format(info.token.decimals, log.args[2], "div")
-                } else {
-                    sniper_text += `🏅 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
-                    total_sniped_volume += format(info.token.decimals, log.args[2], "div")
-                }
-            })
+            if(logs.length <= 10) {
+                logs.forEach((log, index) => {
+                    if(index == 0) {
+                        sniper_text += `🥇 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
+                        total_sniped_volume += format(info.token.decimals, log.args[2], "div")
+                    } else if(index == 1) {
+                        sniper_text += `🥈 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
+                        total_sniped_volume += format(info.token.decimals, log.args[2], "div")
+                    } else if(index == 2) {
+                        sniper_text += `🥉 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
+                        total_sniped_volume += format(info.token.decimals, log.args[2], "div")
+                    } else {
+                        sniper_text += `🏅 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
+                        total_sniped_volume += format(info.token.decimals, log.args[2], "div")
+                    }
+                })
+            } else {
+                const _logs = logs.slice(0, 10)
+
+                _logs.forEach((log, index) => {
+                    if(index == 0) {
+                        sniper_text += `🥇 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
+                        total_sniped_volume += format(info.token.decimals, log.args[2], "div")
+                    } else if(index == 1) {
+                        sniper_text += `🥈 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
+                        total_sniped_volume += format(info.token.decimals, log.args[2], "div")
+                    } else if(index == 2) {
+                        sniper_text += `🥉 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
+                        total_sniped_volume += format(info.token.decimals, log.args[2], "div")
+                    } else {
+                        sniper_text += `🏅 <span class='tg-spoiler'>${log.args[1]}</span> | <span class='tg-spoiler'>${getLocaleStr(format(info.token.decimals, log.args[2], "div"))} ${info.token.symbol}</span>\n\n`
+                        total_sniped_volume += format(info.token.decimals, log.args[2], "div")
+                    }
+                })
+            }
 
             await ctx.replyWithHTML(
-                `<b>💎 ${info.token.name} | ETH 💎</b>\n\n<b>📌 Contract Address:</b><span class='tg-spoiler'>${info.token.address}</span>\n\n<b>🔱 Symbol:</b><span class='tg-spoiler'>$${info.token.symbol}</span>\n\n\n<b>🪙 Token Analytics: ⬇️</b>\n<b>---------------------------</b>\n\n<b>📊 Market Cap:$</b><span class='tg-spoiler'>${mc}</span>\n\n<b>💲 Price:</b><span class='tg-spoiler'>$${_info.pair.priceUsd} | ${_info.pair.priceNative} ${_info.pair.quoteToken.symbol}</span>\n\n<b>📈 PriceChange:</b><span class='tg-spoiler'>5M: ${_info.pair.priceChange.m5} | 1Hr: ${_info.pair.priceChange.h1} | 6Hr: ${_info.pair.priceChange.h6} | 24Hr: ${_info.pair.priceChange.h24}</span>\n\n<b>💸 Volume:</b><span class='tg-spoiler'>5M: ${getLocaleStr(_info.pair.volume.m5)} | 1Hr: ${getLocaleStr(_info.pair.volume.h1)} | 6Hr: ${getLocaleStr(_info.pair.volume.h6)} | 24Hr: ${getLocaleStr(_info.pair.volume.h24)}</span>\n\n<b>♻️ Buys/Sells:</b><span class='tg-spoiler'>5M: ${_info.pair.txns.m5.buys}/${_info.pair.txns.m5.sells} | 1Hr ${_info.pair.txns.h1.buys}/${_info.pair.txns.h1.sells} | 6Hr ${_info.pair.txns.h6.buys}/${_info.pair.txns.h6.sells} | | 24Hr ${_info.pair.txns.h24.buys}/${_info.pair.txns.h24.sells}</span>\n\n<b>💰 Liquidity:</b><span class='tg-spoiler'>$${getLocaleStr(_info.pair.liquidity.usd)} | ${getLocaleStr(_info.pair.liquidity.base)} ${_info.pair.baseToken.symbol} | ${getLocaleStr(_info.pair.liquidity.quote)} ${_info.pair.quoteToken.symbol}</span>\n\n<b>🍯 Honeypot:</b><span class='tg-spoiler'>${info.honeypotResult.isHoneypot ? "Yes 🚫" : "No ✅"}</span>\n\n<b>🏦 Tax:</b><span class='tg-spoiler'>${Number(info.simulationResult.buyTax).toFixed(2)}% Buy | ${Number(info.simulationResult.sellTax).toFixed(2)}% Sell</span>\n\n<b>🕐 Age:</b><span class='tg-spoiler'>${age}</span>\n\n<b>🛡 Contract Verified:</b><span class='tg-spoiler'>${info.contractCode.openSource ? "Yes ✅" : "No 🚫"}</span>\n\n\n<b>👝 Wallet Insights: ⬇️</b>\n<b>---------------------------</b>\n\n<b>👝 Deployer Wallet:</b><span class='tg-spoiler'>${ca.result[0].contractCreator}</span>\n\n<b>💵 Deployer Balance:</b><span class='tg-spoiler'>${Number(balance).toFixed(3)} ETH</span>\n\n<b>🪠 Clog(% of tokens in the contract):</b><span class='tg-spoiler'>${Number(clog).toFixed(2)}%</span>\n\n\n<b>🚀 Sniper Data: ⬇️</b>\n<b>---------------------------</b>\n\n<b>🔫 ${logs.length} person(s) have sniped in the first block.</b>\n\n${sniper_text}<b>💸 Total Sniper Volume:</b><span class='tg-spoiler'>${getLocaleStr(total_sniped_volume)} ${info.token.symbol}</span>\n\n\n<i>Always DYOR. Scanners are not always 100% accurate.</i>`,
+                `<b>💎 ${info.token.name} | ETH 💎</b>\n\n<b>📌 Contract Address:</b><span class='tg-spoiler'>${info.token.address}</span>\n\n<b>🔱 Symbol:</b><span class='tg-spoiler'>$${info.token.symbol}</span>\n\n\n<b>🪙 Token Analytics: ⬇️</b>\n<b>---------------------------</b>\n\n<b>📊 Market Cap:$</b><span class='tg-spoiler'>${mc}</span>\n\n<b>💲 Price:</b><span class='tg-spoiler'>$${_info.pair.priceUsd} | ${_info.pair.priceNative} ${_info.pair.quoteToken.symbol}</span>\n\n<b>📈 PriceChange:</b><span class='tg-spoiler'>5M: ${_info.pair.priceChange.m5} | 1Hr: ${_info.pair.priceChange.h1} | 6Hr: ${_info.pair.priceChange.h6} | 24Hr: ${_info.pair.priceChange.h24}</span>\n\n<b>💸 Volume:</b><span class='tg-spoiler'>5M: ${getLocaleStr(_info.pair.volume.m5)} | 1Hr: ${getLocaleStr(_info.pair.volume.h1)} | 6Hr: ${getLocaleStr(_info.pair.volume.h6)} | 24Hr: ${getLocaleStr(_info.pair.volume.h24)}</span>\n\n<b>♻️ Buys/Sells:</b><span class='tg-spoiler'>5M: ${_info.pair.txns.m5.buys}/${_info.pair.txns.m5.sells} | 1Hr ${_info.pair.txns.h1.buys}/${_info.pair.txns.h1.sells} | 6Hr ${_info.pair.txns.h6.buys}/${_info.pair.txns.h6.sells} | | 24Hr ${_info.pair.txns.h24.buys}/${_info.pair.txns.h24.sells}</span>\n\n<b>💰 Liquidity:</b><span class='tg-spoiler'>$${getLocaleStr(_info.pair.liquidity.usd)} | ${getLocaleStr(_info.pair.liquidity.base)} ${_info.pair.baseToken.symbol} | ${getLocaleStr(_info.pair.liquidity.quote)} ${_info.pair.quoteToken.symbol}</span>\n\n<b>🍯 Honeypot:</b><span class='tg-spoiler'>${info.honeypotResult.isHoneypot ? "Yes 🚫" : "No ✅"}</span>\n\n<b>🏦 Tax:</b><span class='tg-spoiler'>${Number(info.simulationResult.buyTax).toFixed(2)}% Buy | ${Number(info.simulationResult.sellTax).toFixed(2)}% Sell</span>\n\n<b>🕐 Age:</b><span class='tg-spoiler'>${age}</span>\n\n<b>🛡 Contract Verified:</b><span class='tg-spoiler'>${info.contractCode.openSource ? "Yes ✅" : "No 🚫"}</span>\n\n\n<b>👝 Wallet Insights: ⬇️</b>\n<b>---------------------------</b>\n\n<b>👝 Deployer Wallet:</b><span class='tg-spoiler'>${ca.result[0].contractCreator}</span>\n\n<b>💵 Deployer Balance:</b><span class='tg-spoiler'>${Number(balance).toFixed(3)} ETH</span>\n\n<b>🪠 Clog(% of tokens in the contract):</b><span class='tg-spoiler'>${Number(clog).toFixed(2)}%</span>\n\n\n<b>🚀 Sniper Data: ⬇️</b>\n<b>---------------------------</b>\n\n<b>🔫 ${logs.length} person(s) sniped in the first block.</b>\n\n${sniper_text}<b>💸 Total Sniper Volume:</b><span class='tg-spoiler'>${getLocaleStr(total_sniped_volume)} ${info.token.symbol}</span>\n\n\n<i>Always DYOR. Scanners are not always 100% accurate.</i>`,
                 {
                     parse_mode : "HTML",
                     ...Markup.inlineKeyboard([
-                        [Markup.button.url("Website", _info.pair.info.websites.length > 0 ? _info.pair.info.websites[0].url : "https://t.me")],
-                        [Markup.button.url("Twitter", _info.pair.info.socials.length > 0 ? _info.pair.info.socials[0].url : "https://t.me")],
-                        [Markup.button.url("Telegram", _info.pair.info.socials.length > 1 ? _info.pair.info.socials[1].url : "https://t.me")],
+                        [Markup.button.url("Website", "info" in _info.pair && "websites" in _info.pair.info && _info.pair.info.websites.length > 0 ? _info.pair.info.websites[0].url : "https://t.me")],
+                        [Markup.button.url("Twitter", "info" in _info.pair && "socials" in _info.pair.info && _info.pair.info.socials.length > 0 ? _info.pair.info.socials[0].url : "https://t.me")],
+                        [Markup.button.url("Telegram", "info" in _info.pair && "socials" in _info.pair.info && _info.pair.info.socials.length > 1 ? _info.pair.info.socials[1].url : "https://t.me")],
                         [Markup.button.url("Chart", _info.pair.url)]
                     ])
                 }
